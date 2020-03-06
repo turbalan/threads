7f7818c398aee12c2771bda0a203b2dabb4a3438
Pavel Kuts <pavkuts@gmail.com>
2020-03-06 17:55:39 +0200

    Getting a response from json-server; Timedate component

--- src/Threads.jsx
+++ src/Threads.jsx
@@ -3,27 +3,23 @@ import React, { useEffect, useState } from "react";
 import Thread from "./Thread";
 
 const Threads = () => {
-  const [threads, setThreads] = useState({});
-  const server = "http://localhost:3000/threads/";
-
-  async function getThreads(url) {
-    const response = await fetch(url);
-    const json = await response.json();
-    setThreads(json);
-    return json;
-  }
-
-  const threadsAPI = getThreads(server);
-  console.log(threadsAPI);
+  const [threads, setThreads] = useState([]);
+  const server = "http://localhost:3000/threads";
 
   useEffect(() => {
-    getThreads(server);
-  }, [threads]);
+    fetch(server).then(response =>
+      response.json().then(result => {
+        setThreads([result]);
+      })
+    );
+  }, []);
 
   return (
     <article className="threads">
-      {threadsAPI.map(thread => {
-        return <Thread thread={thread} key={thread[0].thread_id} />;
+      {threads.map(thread => {
+        return thread.map(item => {
+          return <Thread thread={item} key={item[0].thread_id} />;
+        });
       })}
     </article>
   );
