91a7c00d8804c009b94a71a843e797b7feeff212
Pavel Kuts <pavkuts@gmail.com>
2020-03-06 16:08:22 +0200

    json-server installed; not reading the promise from the server

--- src/Threads.jsx
+++ src/Threads.jsx
@@ -1,11 +1,28 @@
-import React from "react";
-import data from "../data/data.json";
+import React, { useEffect, useState } from "react";
+// import data from "../data/data.json";
 import Thread from "./Thread";
 
 const Threads = () => {
+  const [threads, setThreads] = useState({});
+  const server = "http://localhost:3000/threads/";
+
+  async function getThreads(url) {
+    const response = await fetch(url);
+    const json = await response.json();
+    setThreads(json);
+    return json;
+  }
+
+  const threadsAPI = getThreads(server);
+  console.log(threadsAPI);
+
+  useEffect(() => {
+    getThreads(server);
+  }, [threads]);
+
   return (
     <article className="threads">
-      {data.threads.map(thread => {
+      {threadsAPI.map(thread => {
         return <Thread thread={thread} key={thread[0].thread_id} />;
       })}
     </article>
